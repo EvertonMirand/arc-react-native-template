31e8ed829c3d9ec9c6579ab2ffcd4ff71ee7b16f
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2020-01-27 22:39:05 -0300

    Ajusta a navegação da pagina Home

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,42 +1,34 @@
-
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
 import Home from '~/views/home/Home.screen';
 import DetailCharacter from '~/views/detail/DetailCharacter.screen';
 
-
-const Routes = createAppContainer(
-    createStackNavigator(
-        {
-            Home: {
-                screen: Home,
-                navigationOptions: ({ navigation }) => ({
-                    title: 'Rick & Morty',
-                }),
-            },
-            DetailCharacter: {
-                screen: DetailCharacter,
-                navigationOptions: ({ navigation }) => ({
-                    title: 'Rick & Morty',
-                    // title: navigation.state.params.character.name,
-                }),
-            },
-        },
-        {
-            defaultNavigationOptions: {
-                headerStyle: {
-                    // backgroundColor: colors.primary,
-                    elevation: 0,
-                },
-                headerTintColor: '#FFF',
-                headerTitleStyle: {
-                    fontWeight: 'bold',
-                    // color: colors.text,
-                },
-            },
-        },
-    ),
+const App = createStackNavigator(
+  {
+    Home: {
+      screen: Home,
+      title: 'Rick & Morty',
+    },
+    DetailCharacter,
+  },
+  {
+    defaultNavigationOptions: {
+      title: 'Rick & Morty',
+      headerStyle: {
+        backgroundColor: 'rgb(57, 190, 140)',
+        elevation: 0,
+      },
+      headerTintColor: '#FFF',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+        // color: colors.text,
+      },
+    },
+    initialRouteName: 'Home',
+  },
 );
+
+const Routes = createAppContainer(App);
 
 export default Routes;
--- src/views/home/Home.screen.js
+++ src/views/home/Home.screen.js
@@ -1,78 +1,74 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { LoadingFooter, Container } from '~/components/ui'
-import { GetListCharacter } from '~/services/HomeService'
-import { styles } from './Home.styles'
-import { FlatList } from 'react-native';
+import React, {useState, useEffect, useContext} from 'react';
+import {LoadingFooter, Container} from '~/components/ui';
+import {GetListCharacter} from '~/services/HomeService';
+import {styles} from './Home.styles';
+import {FlatList} from 'react-native';
 import CharacterCell from '~/components/cells/character/CharacterCell';
 
-const Home: () => React$Node = () => {
-    const [loading, setLoading] = useState(false);
-    const [nextPage, setNextPage] = useState('');
-    const [listCharacter, setListCharacter] = useState([]);
+const Home = ({navigation}) => {
+  const [loading, setLoading] = useState(false);
+  const [nextPage, setNextPage] = useState('');
+  const [listCharacter, setListCharacter] = useState([]);
 
-    useEffect(() => {
-        getListCharacter();
-    }, [])
+  useEffect(() => {
+    getListCharacter();
+  }, []);
 
-    /**
-     * Request Service 
-     */
-    const getListCharacter = () => {
+  /**
+   * Request Service
+   */
+  const getListCharacter = () => {
+    setLoading(!loading);
+    GetListCharacter(nextPage)
+      .then(response => response.data)
+      .then(response => {
+        let list = [...listCharacter, ...response.results];
+        setListCharacter(list);
+        setNextPage(response.info.next);
         setLoading(!loading);
-        GetListCharacter(nextPage)
-            .then(response => response.data)
-            .then(response => {
-                let list = [...listCharacter, ...response.results];
-                setListCharacter(list);
-                setNextPage(response.info.next);
-                setLoading(!loading);
-            })
-            .catch(error => {
-                setLoading(!loading);
-                console.warn(error);
-            });
-    }
+      })
+      .catch(error => {
+        setLoading(!loading);
+        console.warn(error);
+      });
+  };
 
-    const showDetails = (_id) => {
-        // const navigation = useContext(NavigationContext);
-        console.warn(props);
-        // console.warn(navigation);
-        navigation.navigate('DetailCharacter')
-        // navigate('DetailCharacter', { id: _id });
-        // navigation.navigate('DetailCharacter', { id: _id });
+  const showDetails = _id => {
+    navigation.navigate('DetailCharacter', {id: _id});
+  };
 
-    }
+  //----------------- Render View --------------------------
 
-    //----------------- Render View --------------------------
-
-    const renderItem = ({ item }) => {
-        return (
-            <CharacterCell
-                id={item.id}
-                name={item.name}
-                sourceImage={item.image}
-                onPress={showDetails}
-            />
-        );
-    }
-
-    const keyExtractor = (item, index) => `${index}`;
-
-    const renderFooter = () => { return (<LoadingFooter loading={loading} />) };
-
+  const renderItem = ({item}) => {
     return (
-        <Container>
-            <FlatList
-                style={styles.list}
-                onEndReached={getListCharacter}
-                onEndReachedThreshold={0.1}
-                data={listCharacter}
-                keyExtractor={keyExtractor}
-                renderItem={renderItem}
-                ListFooterComponent={renderFooter}
-            />
-        </Container>
+      <CharacterCell
+        id={item.id}
+        name={item.name}
+        sourceImage={item.image}
+        onPress={showDetails}
+      />
     );
+  };
+
+  const keyExtractor = (item, index) => `${index}`;
+
+  const renderFooter = () => {
+    return <LoadingFooter loading={loading} />;
+  };
+
+  return (
+    <Container>
+      <FlatList
+        style={styles.list}
+        onEndReached={getListCharacter}
+        onEndReachedThreshold={0.1}
+        data={listCharacter}
+        keyExtractor={keyExtractor}
+        renderItem={renderItem}
+        ListFooterComponent={renderFooter}
+      />
+    </Container>
+  );
 };
 
 /**
