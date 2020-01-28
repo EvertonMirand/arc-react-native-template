d2d1c8ac28732b6471e36a5c0a7395e25b24ef49
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2020-01-27 22:39:23 -0300

    Configura a DetailCharacter para usar hooks

--- src/views/detail/DetailCharacter.screen.js
+++ src/views/detail/DetailCharacter.screen.js
@@ -8,101 +8,105 @@
  * @memberof DetailCharacter
  */
 
-import React, { Component } from 'react';
-import { View, Text, Image, ScrollView } from 'react-native';
-import { styles } from './DetailCharacter.styles';
-import { GetCharacter } from '~/services/DetailCharacterService';
+import React, {useState, useEffect} from 'react';
+import {View, Text, Image, ScrollView} from 'react-native';
+import {styles} from './DetailCharacter.styles';
+import {GetCharacter} from '~/services/DetailCharacterService';
 import Container from '~/components/ui/container/Container';
 
-export default class DetailCharacter extends Component {
+const DetailCharacter = ({navigation}) => {
+  const {
+    container,
+    image,
+    textName,
+    containerTransparent,
+    textInfo,
+    textDescription,
+    viewDescription,
+  } = styles;
 
-    static navigationOptions = { header: null };
+  const [character, setCharacter] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(true);
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            character: '',
-            isLoading: true,
-        };
-        this.getCharacter(this.props.navigation.state.params.id);
-    }
+  useEffect(() => {
+    getCharacter(navigation.state.params.id);
+  }, []);
 
-    getCharacter = id => {
-        GetCharacter(id)
-            .then(response => response.data)
-            .then(response => {
-                this.setState({
-                    character: response,
-                    isLoading: false,
-                });
-            })
-            .catch(error => {
-                this.setState({ isLoading: false });
-            });
-    }
+  const getCharacter = id => {
+    GetCharacter(id)
+      .then(response => response.data)
+      .then(response => {
+        setCharacter(response);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        setIsLoading(false);
+      });
+  };
 
-    getData(data) {
-        var returnData = '';
-        var returnData = data.split('T');
-        return returnData[0];
-    }
+  const getData = data => {
+    var returnData = '';
+    var returnData = data.split('T');
+    return returnData[0];
+  };
 
-    /**
-     * @param {string} title 
-     * @param {string} description 
-     */
-    renderForms(title, description) {
-        const { textInfo, textDescription, viewDescription } = styles;
-        return (
-            <View style={viewDescription}>
-                <Text style={textInfo}>{title}</Text>
-                <Text style={textDescription}>{description}</Text>
-            </View>
-        );
-    }
+  /**
+   * @param {string} title
+   * @param {string} description
+   */
+  const renderForms = (title, description) => {
+    return (
+      <View style={viewDescription}>
+        <Text style={textInfo}>{title}</Text>
+        <Text style={textDescription}>{description}</Text>
+      </View>
+    );
+  };
 
-    render() {
-        const { container, image, textName, containerTransparent } = styles;
-        const { character } = this.state;
+  return (
+    <Container>
+      <View>
+        {character && (
+          <View style={container}>
+            <ScrollView>
+              <View style={containerTransparent}>
+                <Image
+                  source={{uri: character.image}}
+                  resizeMode="contain"
+                  style={image}
+                />
+                <Text style={textName}>{character.name}</Text>
 
-        return (
-            <Container>
-                <View>
-                    {character && (
-                        <View style={container}>
-                            <ScrollView>
-                                <View style={containerTransparent}>
-                                    <Image
-                                        source={{ uri: character.image }}
-                                        resizeMode='contain'
-                                        style={image}
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                  }}>
+                  {renderForms('Status:', character.status)}
+                  {renderForms('Specie:', character.species)}
+                </View>
 
-                                    />
-                                    <Text style={textName}>{character.name}</Text>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                  }}>
+                  {renderForms('Created:', getData(character.created))}
+                  {renderForms('Gender:', character.gender)}
+                </View>
+                {renderForms('Origin:', character.origin.name)}
+                {renderForms('Location:', character.location.name)}
+              </View>
+            </ScrollView>
+          </View>
+        )}
+      </View>
+    </Container>
+  );
+};
 
-                                    <View style={{
-                                        flexDirection: 'row',
-                                        justifyContent: 'space-between',
-                                    }}>
-                                        {this.renderForms('Status:', character.status)}
-                                        {this.renderForms('Specie:', character.species)}
-                                    </View>
+DetailCharacter.navigationOptions = {
+  header: null,
+};
 
-                                    <View style={{
-                                        flexDirection: 'row',
-                                        justifyContent: 'space-between',
-                                    }}>
-                                        {this.renderForms('Created:', this.getData(character.created))}
-                                        {this.renderForms('Gender:', character.gender)}
-                                    </View>
-                                    {this.renderForms('Origin:', character.origin.name)}
-                                    {this.renderForms('Location:', character.location.name)}
-                                </View>
-                            </ScrollView>
-                        </View>
-                    )}
-                </View >
-            </Container>
-        );
-    }
-}
+export default DetailCharacter;
